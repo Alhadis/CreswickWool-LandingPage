fc85c80faa0ff438c42b4b3895b57e9aa72b0b4c
Alhadis <gardnerjohng@gmail.com>
2015-04-13 16:04:10 +1000

    Polish API file's whitespace a bit
    Sorry, Raul.

--- api.php
+++ api.php
@@ -4,61 +4,47 @@
  * Endpoint to manage cart content on an external page
  */
 
-/*
-Load Magento
-*/
-require_once ("../app/Mage.php");
+ 
+# Load Magento
+require_once '../app/Mage.php';
 umask(0);
-Mage::app("default");
-Mage::getSingleton("core/session", array("name" => "frontend"));
+Mage::app('default');
+Mage::getSingleton('core/session', array('name' => 'frontend'));
 
-/*
-Load cart
-*/
+# Load cart
 $cart = Mage::getSingleton('checkout/cart');
 
-/*
-Set up response
-*/
+# Set up response
 $response = array('message' => '', 'status' => 200);
 
-/*
-Manage requests
-*/
+# Manage requests
 
-// Add to cart
-if ($_POST['action'] == 'add_to_cart') {
-	
-	try {
+# Add to cart
+if($_POST['action'] === 'add_to_cart'){
 
-		$product_id = $_POST['product_id'];
-		$quantity = $_POST['quantity'];
-		
+	try{
+		$product_id		=	$_POST['product_id'];
+		$quantity		=	$_POST['quantity'];
+
 		$cart->addProduct($product_id, array('qty' => $quantity));
 		$cart->save();
-		
+
 		Mage::getSingleton('checkout/session')->setCartWasUpdated(true);
-
-		$response['message'] = 'Added product '.$product_id.' with quantity '.$quantity.' into cart';
-
+		$response['message']	=	sprintf('Added product %1$s with quantity %2$s into cart', $product_id, $quantity);
 	}
-	catch(Exception $e) {
 
-		$response['message'] = 'Unable to add product to cart. Error: ' . $e;
-		$response['status'] = 400;
-	
+	catch(Exception $e){
+		$response['message']	=	'Unable to add product to cart. Error: '.$e;
+		$response['status']		=	400;
 	}
 
 } 
-else {
 
-	$response['message'] = 'No action specified';
-	$response['status'] = 400;
-
+else{
+	$response['message']	=	'No action specified';
+	$response['status']		=	400;
 }
 
-/*
-Respond
-*/
+# Respond
 echo json_encode($response);
 http_response_code($response['status']);
